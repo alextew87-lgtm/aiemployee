141a0f0b2fdfce87e5ac1a070289e74085d90677
alextew87-lgtm <alextew87@gmail.com>
2025-09-23 00:15:32 +0300

    Update consent-exec.v1.js

--- assets/js/consent-exec.v1.js
+++ assets/js/consent-exec.v1.js
@@ -41,12 +41,46 @@
 
   // 1) авто-запуск при наличии сохранённого согласия
   document.addEventListener('DOMContentLoaded', function(){ exec(read()); });
+  
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104158481', 'ym');
+
+    ym(104158481, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/104158481" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
+
+
 
   // 2) запуск сразу после кликов «Принять/Улучшенный опыт/Сохранить выбор»
   ['cookie-accept-all','cookie-save-master','cookie-save-custom'].forEach(function(id){
     var btn=document.getElementById(id);
     if (btn) btn.addEventListener('click', execSoon, { passive:true });
   });
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104158481', 'ym');
+
+    ym(104158481, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/104158481" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
+
+
 
   // опционально: ручной вызов из консоли
   window.__runConsentLoaders=function(){ exec(read()); };
